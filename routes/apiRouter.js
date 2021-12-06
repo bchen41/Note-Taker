4f6eb567c2f2ad290d28493951128b7fb69b8a24
bchen41 <bettychen41@outlook.com>
2021-12-06 15:51:07 -0500

    created GET route to read updated db.json file&return saved notes

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -5,4 +5,9 @@ const { v4: uuidv4 } = require("uuid");
 
 const apiRouter = require("express").Router();
 
+apiRouter.get("/notes", (req, res) => {
+  db = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+  res.json(db);
+});
+
 module.exports = apiRouter;
