099b416f037f890219552213b3f84b6f301da447
bchen41 <bettychen41@outlook.com>
2021-12-06 15:26:27 -0500

    created pathway to api routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const htmlRouter = require("./routes/htmlRouter");
+const apiRouter = require("./routes/apiRouter");
 
 const PORT = 3001;
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/", htmlRouter);
+app.use("/api", apiRouter);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
