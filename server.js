520397795c0b9c745dbe33973cf8e9ecf3ddad88
bchen41 <bettychen41@outlook.com>
2021-12-06 15:46:46 -0500

    updated PORT for heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const htmlRouter = require("./routes/htmlRouter");
 const apiRouter = require("./routes/apiRouter");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
